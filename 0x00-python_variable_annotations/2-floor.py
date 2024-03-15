1726e949b1da8ff4c1ff9e29d1f3d4354f83b010
Christian <111076892+i-christian@users.noreply.github.com>
2024-03-15 16:18:49 +0200

    add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+"""
+.2-floor.py
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Takes a float n as argument and returns its floor
+    """
+    return math.floor(n)
